a69ee14eb24cd0622cdb303a4a7aae6069825f89
Jasmin Mediani <molihua1992@gmail.com>
2023-09-17 12:04:02 +0200

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function Home () {
   return (
-    <main className="">
-
-      Homepage
-
+    <main className='px-[50px]'>
+      <nav className='h-[70px] py-[15px] lg:h-[100px] flex justify-between bg-orange-500'>
+        <img
+          src='https://picsum.photos/200'
+          alt=''
+          className='h-[61px] w-[220px]'
+        />
+        <ul className='flex bg-red-500 gap-[20px]'>
+          <li>Homepage</li>
+          <li>Chi sono</li>
+          <li>Servizi</li>
+          <li>Galleria</li>
+          <li>Contatti</li>
+        </ul>
+      </nav>
     </main>
   )
 }
