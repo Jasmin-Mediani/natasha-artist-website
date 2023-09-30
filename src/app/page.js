9b9c183896cb848c4ced9a06275496bac9945a05
Jasmin Mediani <molihua1992@gmail.com>
2023-09-30 15:18:14 +0200

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home () {
     <div className="flex gap-20 justify-between w-[95%] mx-auto pt-[100px]">
       <img src="https://picsum.photos/750/500?grayscale" alt="" className="w-[750px] h-[500px]"/>
       <div className="self-end flex flex-col content-between">
-        <div className="bg-[#0EB2E7] h-[165px] p-[30px] text-white flex items-center text-[75px] mb-20">Natasha Sood</div>
+        <div className="bg-[#0EB2E7] h-[165px] p-[30px] text-white flex items-center lg:text-[75px] mb-20">Natasha Sood</div>
         <h2 className="lg:text-[42px] mb-6">Lorem ipsum dolor sit amet</h2>
         <div className="mb-[80px]">Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.</div>
 
