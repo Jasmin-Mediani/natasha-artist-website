28885246ba7aa7a04e444b505470fee1450a25b9
Jasmin Mediani <molihua1992@gmail.com>
2023-10-15 09:00:29 +0200

    wip

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,13 @@ export default function Home () {
 
           
           <div className="flex flex-col content-between relative h-inherit">
-            <div className="bg-[#0EB2E7] h-[165px] p-[30px] text-white flex items-center lg:text-[75px] mb-20 absolute top-5 -left-[250px] lg:w-[calc(100vh + 250px)]">Natasha Sood</div>
+            {/* Banner lg */}
+            <div className="hidden lg:block bg-[#0EB2E7] h-[165px] p-[30px] text-white flex items-center lg:text-[75px] mb-20 absolute top-5 -left-[250px]" style={{ width: "calc(100% + 250px)" }} >Natasha Sood</div>
+
+            {/* Banner md */}
+            {/* Banner sm */}
+
+            {/* Page main paragraph */}
             <h2 className="lg:text-[42px] mb-6 lg:mt-[250px]">Lorem ipsum dolor sit amet</h2>
             <div className="mb-[80px]">Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna dictum porta.</div>
 
