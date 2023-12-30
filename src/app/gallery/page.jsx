89a4fe285e0b6645d5f434885655fed835b18383
Jasmin Mediani <molihua1992@gmail.com>
2023-12-30 12:38:56 +0100

    wip

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,47 +1,55 @@
 'use client'
 
-import { useEffect, useState } from "react"
-import {categorie} from "./dati"
-import Sezione from "./Sezione"
+import { useEffect, useState } from 'react'
+import { categorie } from './dati'
+import Sezione from './Sezione'
 
+export default function Galleria () {
+  const [primaCategoriaDisponibile, setPrimaCategoriaDisponibile] = useState({})
+  const [nomeCategoriaSelezionata, setNomeCategoriaSelezionata] = useState('')
+  const [categoriaSelezionata, setCategoriaSelezionata] = useState({})
 
+  //montaggio della prima categoria come default
+  useEffect(() => {
+    setPrimaCategoriaDisponibile(Object.keys(categorie)[0])
+    setNomeCategoriaSelezionata(primaCategoriaDisponibile)
+    setCategoriaSelezionata(categorie[0])
+  }, [categorie])
 
-export default function Galleria() {
+  return (
+    <main className='w-full bg-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px]'>
+      <div className='mx-auto max-w-[1350px]'>
+        <h2 className='lg:text-[42px] mb-6 text-center'> Gallery </h2>
 
-	const [primaCategoriaDisponibile, setPrimaCategoriaDisponibile] = useState({});
-	const [nomeCategoriaSelezionata, setNomeCategoriaSelezionata] = useState("");
-	const [categoriaSelezionata, setCategoriaSelezionata] = useState({});
+        <div className='flex gap-16 flex justify-center flex-col lg:flex-row'>
+          <div className='lg:w-fit max-w-[330px] bg-white p-4 lg:h-fit'>
+            <ul>
+              {Object.entries(categorie).map(([nomeCategoria, obj]) => (
+                <li
+                  className='flex justify-between'
+                  onClick={() => {
+                    setNomeCategoriaSelezionata(nomeCategoria),
+                      setCategoriaSelezionata(categorie[nomeCategoria])
+                  }}
+                  key={nomeCategoria}
+                >
+                  <span className='mr-3 mb-2 mt-2'> {nomeCategoria}</span> <span>{obj.opere.length} </span>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-	//montaggio della prima categoria come default
-	useEffect(() => {
-
-		setPrimaCategoriaDisponibile(Object.keys(categorie)[0])
-		setNomeCategoriaSelezionata(primaCategoriaDisponibile);
-		setCategoriaSelezionata(categorie[0])
-	}, [categorie])
-
-
-
-
-	return (
-		<main className='w-full bg-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px] max-w-[1350px]'>
-			<h2 className='lg:text-[42px] mb-6 text-center'> Gallery </h2>
-
-			<div className='flex gap-16 flex justify-center flex-col lg:flex-row'>
-				<div className='lg:w-fit max-w-[330px] bg-white p-4 lg:h-fit'>
-					<ul>
-						{	Object.entries(categorie).map(([nomeCategoria, obj]) => 
-							
-							<li className="flex justify-between" onClick={() => {setNomeCategoriaSelezionata(nomeCategoria), setCategoriaSelezionata(categorie[nomeCategoria])}} key={nomeCategoria}> <span> {nomeCategoria}</span> <span>{obj.opere.length} </span> </li>
-						)} 
-						
-					</ul>
-				</div>
-
-				<div className='lg:w-4/5 bg-white max-w-[1350px]'>
-					<Sezione sezione={categoriaSelezionata != null ? categoriaSelezionata : categorie[primaCategoriaDisponibile]} />
-				</div>
-			</div>
-		</main>
-	)
+          <div className='lg:w-4/5 bg-white max-w-[1350px]'>
+            <Sezione
+              sezione={
+                categoriaSelezionata != null
+                  ? categoriaSelezionata
+                  : categorie[primaCategoriaDisponibile]
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,45 +2,9 @@
 
 export default function Home () {
   return (
-    <div>
-      {/* <main className='flex w-full h-[calc(100vh - 100px)] justify-center lg:justify-normal bg-gradient-to-b from-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px]'>
-        <div className='2xl:flex lg:justify-between lg:mx-auto pt-[0px] w-[95%] items-center lg:items-start justify-center flex flex-col'>
-          <img
-            src='/assets/6X9A6141_BN.png'
-            alt=''
-            className='w-fit'
-          />
-
-          <div
-            className='hidden lg:block bg-[#0EB2E7] h-[165px] p-[30px] text-white flex items-center lg:text-[68px] absolute top-[150px] -t-12'
-            style={{ width: '60vw', right: 0, marginTop: "30px" }}
-          >
-            Natasha Sood
-          </div>
-
-          <div className='flex flex-col content-between relative h-inherit'>
-            <div className='lg:hidden bg-[#0EB2E7] p-[30px] text-white flex items-center text-[40px] mb-[30px] max-w-[750px] self-center -mt-8 text-center'>
-              Natasha Sood
-            </div>
-            <h2 className='lg:text-[42px] mb-6 mt-[20px] text-[25px] '>
-              Lorem ipsum dolor sit amet
-            </h2>
-            <div className='mb-[40px] text-justify hyphens-auto'>
-              Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec
-              sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id
-              imperdiet et, porttitor at sem. Donec sollicitudin molestie
-              malesuada. Cras ultricies ligula sed magna dictum porta. Quisque
-              velit nisi, pretium ut lacinia in, elementum id enim. Donec rutrum
-              congue leo eget malesuada. Cras ultricies ligula sed magna dictum
-              porta.
-            </div>
-          </div>
-        </div>
-      </main> */}
-
-      <main className='flex w-full h-[calc(100vh - 100px)] justify-center lg:justify-normal bg-gradient-to-b from-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px]'>
+      <main className='flex w-full justify-center lg:justify-normal bg-gradient-to-b from-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px] items-center' style={{ height: "calc(100vh - 100px)" }}>
         <div className='2xl:flex xl:flex-row xl:gap-12 lg:justify-between lg:mx-auto pt-[0px] w-[95%] items-center lg:items-start justify-center flex flex-col'>
-          <img src='/assets/6X9A6141_BN.png' alt='' className='w-fit' />
+          <img src='/assets/6X9A6141_BN.png' alt='' className='w-fit self-center' />
 
           <div className='flex flex-col content-between relative h-inherit'>
             {/* Page main paragraph */}
@@ -76,6 +40,5 @@ export default function Home () {
           </div>
         </div>
       </main>
-    </div>
   )
 }
