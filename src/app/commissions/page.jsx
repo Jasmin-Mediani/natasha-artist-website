e7ac57a88599e6868e2baf5bb52da31e5c216d22
Jasmin Mediani <molihua1992@gmail.com>
2024-05-01 18:19:26 +0200

    wip

--- src/app/commissions/page.jsx
+++ src/app/commissions/page.jsx
@@ -3,7 +3,7 @@ export default function Contatti() {
         <main className="w-full h-[calc(100vh - 100px)] bg-gradient-to-b from-[#F3F3F3] to-[#fff] p-[20px] lg:p-[50px]">
             <h2 className="lg:text-[42px] mb-6 text-center"> Commissions </h2>
 
-
+            <p>If you like my style and would like to discuss a commission, a gift for someone, please, contact me. </p>
         </main>
     )
 }
