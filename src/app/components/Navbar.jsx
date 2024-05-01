9ff8e935708ac2cbece3bb4610801c101a5a4d60
Jasmin Mediani <molihua1992@gmail.com>
2024-05-01 17:50:55 +0200

    wip

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,33 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const useDeviceSize = () => {
+
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  useEffect(() => {
+    // component is mounted and window is available
+    handleWindowResize();
+    window.addEventListener('resize', handleWindowResize);
+    // unsubscribe from the event on component unmount
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  return [width, height]
+
+}
+
 
 export default function Navbar () {
   const [toggleMobileNav, setToggleMobileNav] = useState(false)
+  const [width, height] = useDeviceSize();
+
 
   return (
     <div className='px-[20px] lg:px-[50px] max-w-[1350px] 2xl:mx-auto 2xl:items-center 2xl:justify-center mx-auto'>
@@ -31,7 +56,7 @@ export default function Navbar () {
        <a href="/"> <img
           src='/assets/logo_1.png'
           alt=''
-          style={{width: window.innerWidth < 280 ? "130px" : "144px"}}
+          style={{width: width < 280 ? "130px" : "144px"}}
           className='h-[44px] w-[160px]'
          
           
